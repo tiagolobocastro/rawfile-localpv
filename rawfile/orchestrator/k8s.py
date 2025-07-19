a896b0bebae5285006b93c6445a475292b869933
Muhammed Hussein Karimi <info@karimi.dev>
2025-07-19 10:51:15 +0330

    :zap: Improve volume verifier and task pod to be more safe
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- rawfile/orchestrator/k8s.py
+++ rawfile/orchestrator/k8s.py
@@ -1,50 +1,107 @@
-import json
+from typing import Any, TypedDict
 import uuid
-from pathlib import Path
 from subprocess import CalledProcessError
 from time import sleep
 
 import re
-import yaml
 from datetime import datetime
 from consts import CONFIG, VOLUME_IS_ATTACHED
 from kubernetes import client as k8s_client, config as k8s_config
-from utils.logs import logger, format as log_format, level as log_level
+from utils.logs import LoggingFormats, logger, format as log_format, level as log_level
 
 k8s_config.load_config()
 
 
+class TaskPodInfo(TypedDict):
+    name: str
+    namespace: str
+    datadir: str
+    node_selector: dict[str, str]
+    image_repository: str
+    image_tag: str
+    log_format: LoggingFormats
+    log_level: str
+    reserved_capacity: int | str
+    capacity_override: int
+    command: str
+
+
+def generate_task_pod_manifest(info: TaskPodInfo) -> dict[str, Any]:
+    return {
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {"name": info["name"], "namespace": info["namespace"]},
+        "spec": {
+            "restartPolicy": "Never",
+            "terminationGracePeriodSeconds": 0,
+            "tolerations": [{"operator": "Exists"}],
+            "volumes": [
+                {
+                    "name": "data-dir",
+                    "hostPath": {"path": info["datadir"], "type": "DirectoryOrCreate"},
+                },
+                {"name": "device", "hostPath": {"path": "/dev", "type": "Directory"}},
+            ],
+            "nodeSelector": info["node_selector"],
+            "containers": [
+                {
+                    "name": "task",
+                    "image": f"{info['image_repository']}:{info['image_tag']}",
+                    "imagePullPolicy": "IfNotPresent",
+                    "volumeMounts": [
+                        {"name": "data-dir", "mountPath": "/data"},
+                        {"name": "device", "mountPath": "/dev"},
+                    ],
+                    "resources": {
+                        "requests": {"cpu": "0m", "memory": "0Mi"},
+                        "limits": {"cpu": "100m", "memory": "100Mi"},
+                    },
+                    "env": [
+                        {
+                            "name": "LOG_FORMAT",
+                            "value": str(info["log_format"].value),
+                        },
+                        {"name": "LOG_LEVEL", "value": info["log_level"]},
+                        {
+                            "name": "reserved_capacity",
+                            "value": str(info["reserved_capacity"]),
+                        },
+                        {
+                            "name": "CAPACITY_OVERRIDE",
+                            "value": str(info["capacity_override"]),
+                        },
+                    ],
+                    "command": ["/bin/sh", "-c"],
+                    "args": [info["command"]],
+                }
+            ],
+        },
+    }
+
+
 def volume_to_node(volume_id):
     api = k8s_client.CoreV1Api()
-    pv = api.read_persistent_volume(name=volume_id)
+    pv: k8s_client.V1PersistentVolume = api.read_persistent_volume(name=volume_id)
+    curr_node_affinity: k8s_client.V1VolumeNodeAffinity = pv.spec.node_affinity
     node_name = (
-        pv.spec.node_affinity.required.node_selector_terms[0]
+        curr_node_affinity.required.node_selector_terms[0]
         .match_expressions[0]
         .values[0]
     )
-    assert all(
-        (
-            len(pv.spec.node_affinity.required.node_selector_terms) == 1,
-            len(pv.spec.node_affinity.required.node_selector_terms[0].match_expressions)
-            == 1,
-            len(
-                pv.spec.node_affinity.required.node_selector_terms[0]
-                .match_expressions[0]
-                .values
-            )
-            == 1,
-            pv.spec.node_affinity.required.node_selector_terms[0]
-            .match_expressions[0]
-            .key
-            == "hostname",
-            pv.spec.node_affinity.required.node_selector_terms[0]
-            .match_expressions[0]
-            .operator
-            == "In",
-            pv.spec.node_affinity.required.node_selector_terms[0].match_fields
-            in (None, []),
+    expected_affinity = k8s_client.V1VolumeNodeAffinity(
+        required=k8s_client.V1NodeSelector(
+            [
+                k8s_client.V1NodeSelectorTerm(
+                    match_expressions=[
+                        k8s_client.V1NodeSelectorRequirement(
+                            key="hostname", operator="In", values=[node_name]
+                        )
+                    ]
+                )
+            ]
         )
     )
+    assert curr_node_affinity == expected_affinity
     return node_name
 
 
@@ -63,11 +120,11 @@ def run_on_node(fn, node):
     name = f"task-{uuid.uuid4()}"
     registry = CONFIG["image_registry"]
     repository = CONFIG["image_repository"]
-    ctx = {
+    ctx: TaskPodInfo = {
         "name": name,
         "namespace": CONFIG["namespace"],
-        "nodeSelector": json.dumps({"kubernetes.io/hostname": node}),
-        "cmd": json.dumps(fn),
+        "node_selector": {"kubernetes.io/hostname": node},
+        "command": fn,
         "image_repository": (
             f"{registry}/{repository}" if registry is not None else repository
         ),
@@ -78,18 +135,17 @@ def run_on_node(fn, node):
         "reserved_capacity": CONFIG["reserved_capacity"],
         "capacity_override": CONFIG.get("capacity_override", 0),
     }
-    template = Path("./templates/task.yaml").read_bytes().decode()
-    manifest = template.format(**ctx)
-    obj = yaml.safe_load(manifest)
+    manifest = generate_task_pod_manifest(ctx)
+    logger.debug("Creating task pod", manifest=manifest)
     api.create_namespaced_pod(
         namespace=CONFIG["namespace"],
-        body=obj,
+        body=manifest,
     )
 
     def is_finished():
         task_pod = api.read_namespaced_pod(name=name, namespace=CONFIG["namespace"])
         status = task_pod.status
-        if status.phase in ["Succeeded", "Failed"]:
+        if status.phase in ("Succeeded", "Failed"):
             return True
         return False
 
@@ -103,7 +159,9 @@ def run_on_node(fn, node):
 
     if status.phase != "Succeeded":
         exit_code = status.container_statuses[0].state.terminated.exit_code
-        logger.error("run_on_node task failed", exit_code=exit_code, output=logs)
+        logger.error(
+            "Task failed", exit_code=exit_code, output=logs, pod_name=name, node=node
+        )
         raise CalledProcessError(returncode=exit_code, cmd=f"Task: {name}", output=logs)
 
     match = re.search(f"{VOLUME_IS_ATTACHED}=(True|False)", logs)
