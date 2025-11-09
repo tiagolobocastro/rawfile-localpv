26d69c161fd6591feb5ed52d616e33bc27d02ddd
Muhammed Hussain Karimi <info@karimi.dev>
2025-11-09 19:49:48 +0330

    :test_tube: Adding trivy for container scanning
    Signed-off-by: Muhammed Hussain Karimi <info@karimi.dev>

---
+++ .ci/trivy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "$0")"
+
+set -exuo pipefail
+source "$SCRIPT_DIR/common"
+
+command -v trivy || (
+  echo 'Trivy is not installed. Aborting.'
+  exit 1
+)
+export CI_TEST=true
+"$SCRIPT_DIR"/build.sh
+
+export TRIVY_OUTPUT=./trivy-report.sarif
+export TRIVY_FORMAT=sarif
+export TRIVY_SEVERITY="HIGH,CRITICAL"
+export TRIVY_PKG_TYPES="os,library"
+export TRIVY_IGNORE_UNFIXED=false
+export TRIVY_SCANNERS="vuln,secret,misconfig"
+
+trivy image $(build-image-uri $CI_TEST_IMAGE_TAG)
